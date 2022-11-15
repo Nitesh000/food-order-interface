68f495daa1bebfe0c59348b4e1c7587f208382be
Nitesh00 <ntudu040@gmail.com>
2022-11-15 13:03:01 +0530

    adding mealitemform: number of meals

---
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,23 @@
+import Input from "../../UI/Input";
+import classes from "./MealItemForm.module.css";
+
+const MealItemForm = (props) => {
+  return (
+    <form className={classes.form}>
+      <Input
+        label="Amount"
+        input={{
+          id: "amount",
+          type: "numbers",
+          min: "1",
+          max: "5",
+          strp: "1",
+          defaultValue: "1"
+        }}
+      />
+      <button>+ Add</button>
+    </form>
+  );
+};
+
+export default MealItemForm;
